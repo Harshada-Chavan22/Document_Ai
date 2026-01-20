8ee703fee78b49d599d8fb2e60ef55c4ea45a6e1
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-01-20 23:53:38 +0530

    pdf2image path setting

--- utils/pdf_to_image.py
+++ utils/pdf_to_image.py
@@ -2,10 +2,16 @@
 from pdf2image import convert_from_path
 import os
 
+POPPLER_PATH = r"C:\Users\harsh\Downloads\Release-25.12.0-0\poppler-25.12.0\Library\bin"
+
 def pdf_to_images(pdf_path):
     print(f"[INFO] Converting PDF to images: {pdf_path}")
 
-    images = convert_from_path(pdf_path, dpi=300)
+    images = convert_from_path(
+        pdf_path,
+        dpi=300,
+        poppler_path=POPPLER_PATH
+    )
 
     os.makedirs("data/images", exist_ok=True)
     image_paths = []
--- vision/dealer_signature_stamp.py
+++ vision/dealer_signature_stamp.py
@@ -1,23 +1,15 @@
 # vision/dealer_signature_stamp.py
-from ultralytics import YOLO
-
-model = YOLO("models/best.pt")
 
 def detect_signature_stamp(image_paths):
-    signature = {"present": False, "bbox": None}
-    stamp = {"present": False, "bbox": None}
+    print("[INFO] Skipping YOLO detection (model not trained yet)")
 
-    for img_path in image_paths:
-        results = model(img_path)
-
-        for r in results:
-            for box in r.boxes:
-                cls = int(box.cls[0])
-                bbox = box.xyxy[0].tolist()
-
-                if cls == 0:
-                    signature = {"present": True, "bbox": bbox}
-                elif cls == 1:
-                    stamp = {"present": True, "bbox": bbox}
-
-    return {"signature": signature, "stamp": stamp}
+    return {
+        "signature": {
+            "present": False,
+            "bbox": None
+        },
+        "stamp": {
+            "present": False,
+            "bbox": None
+        }
+    }
