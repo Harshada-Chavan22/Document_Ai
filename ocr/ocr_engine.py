46bece322904047f7fedc732780afa02dd98d48e
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-01-21 13:08:52 +0530

    added tesseract

--- ocr/ocr_engine.py
+++ ocr/ocr_engine.py
@@ -1,26 +1,14 @@
-# ocr/ocr_engine.py
+import os
+os.environ["FLAGS_use_onednn"] = "0"
+
 from paddleocr import PaddleOCR
-import cv2
 
-ocr = PaddleOCR(use_angle_cls=True, lang="en")
+ocr = PaddleOCR(lang='en')
 
-def run_ocr(image_paths):
-    print("[INFO] Running OCR")
-    ocr_data = []
-
-    for img_path in image_paths:
-        img = cv2.imread(img_path)
-        result = ocr.ocr(img, cls=True)
-
-        for line in result[0]:
-            bbox = line[0]
-            text = line[1][0]
-            conf = line[1][1]
-
-            ocr_data.append({
-                "text": text,
-                "bbox": bbox,
-                "confidence": conf
-            })
-
-    return ocr_data
+def run_ocr(images):
+    ocr_results = []
+    for img in images:
+        result = ocr.ocr(img)
+        if result and len(result) > 0:
+            ocr_results.extend(result[0])
+    return ocr_results
